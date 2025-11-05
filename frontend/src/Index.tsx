2a7d166fbdecdfcb300bb34f3af01a52b2fe4577
Charly <carlosr.gomez@udem.edu>
2025-11-05 12:19:11 -0600

    Se añadio función con cognito

--- frontend/src/Index.tsx
+++ frontend/src/Index.tsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 import Dashboard from "./Dashboard";
 
 const Index = () => {
---
+++ frontend/src/aws-exports.ts
@@ -0,0 +1,11 @@
+const awsmobile = {
+  Auth: {
+    region: "us-east-2", // tu región AWS
+    userPoolId: "us-east-2_cNYSlsmC0", // tu User Pool ID
+    userPoolWebClientId: "4386f3iqvgb0bte5ahl61qd6hf", // App client ID (SPA)
+    authenticationFlowType: "USER_SRP_AUTH",
+  },
+};
+
+
+export default awsmobile;
